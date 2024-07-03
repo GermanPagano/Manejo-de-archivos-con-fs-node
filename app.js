43785da979171a0846579eff4ffef0f06e6ab5c7
germanpagano <sergiogermanpagano@gmail.com>
2024-07-03 20:30:58 -0300

    subiendo js

--- app.js
+++ app.js
@@ -1,1 +1,83 @@
-console.log('objeict') 
+const fs = require('fs')
+const { json } = require('stream/consumers')
+
+class ProductManager {
+    // constructor
+    constructor(filePath){
+        this.path = filePath
+        this.initializeFile()
+        this.nextId = this.getNextID()// acomoda los id segun los productos que tenemos
+
+    }
+
+    // iniciarlizacion del archivo
+    initializeFile(){
+        if(!fs.existsSync(this.path)){
+            fs.writeFileSync(this.patch, JSON.stringify([]))
+        }
+    }
+
+    // funcion para incrementar el id
+    getNextID(){
+        const products = this.getProductsFromFile()
+        if(products.lenght === 0){
+            return 1
+        }
+
+        const maxID = products.reduce((max, product)=> (product.id > max ? product.id : max),0)
+        return maxID +1
+    }
+
+    // metodo para agregar productos
+    addProduct(product){
+        const products = this.getProductsFromFile()
+        product.id = this.nextId
+        this.nextId+=1
+
+        products.push(products)
+        this.saveProductsToFile(products)
+    }
+
+    // metodo para mostrar productos
+    getProducts(){
+        return this.getProductsFromFile()
+    }
+
+    //metodo para mostrar productos por id
+    getProduct(id){
+        const products = this.getProductsFromFile()
+        return products.find(e => e.id === id)
+    }
+
+    // metodo para actualizar o cambiar productos
+    updateProduct(id,updateFields){
+        const products = this.getProductsFromFile()
+        const index = products.findIndex( p => p.id === id)
+        if(index !== -1){
+            products[index] = { ...products[index],updateFields}
+            this.saveProductsToFile(products)
+        }
+    }
+
+    // metodo para eliminar productos por id
+    deleteProduct(id){
+        const products = this.getProductsFromFile()
+        products.filter(p => p.id === id )
+        this.saveProductsToFile(products)
+    }
+
+
+    // metodo para leer el archivo y devuelva todos los productos
+
+    getProductsFromFile(){
+        const data = fs.readFileSync(this.path, "utf-8")
+        return JSON.parse(data)
+    }
+
+    //metodo para guardar productos en un archivo 
+    saveProductsToFile(products){
+        fs.writeFileSync(this.path, JSON.stringify(products,null ,2))
+
+    }
+
+}
